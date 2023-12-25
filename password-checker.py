92658dcfb4ecb67220ee72340f40b526341da803
Kunal <70285661+kvohra93@users.noreply.github.com>
2023-12-24 23:01:40 -0600

    added final code for password-checker.py

--- password-checker.py
+++ password-checker.py
@@ -1,1 +1,8 @@
 # using input function to get user inputs for a username and password and promnpting back the password length to the user
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+password_length = len(password)
+password_mask = '*' * password_length
+print(
+    f"Hello {username}, your password {password_mask} is {password_length} letters long."
+)
