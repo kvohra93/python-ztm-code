5f67ca05479e37ea5b5fb608e68fcd709e556996
Kunal <70285661+kvohra93@users.noreply.github.com>
2023-12-24 23:15:09 -0600

    added code to find duplicate values in a list

--- duplicates.py
+++ duplicates.py
@@ -1,1 +1,11 @@
 #checking and returning duplicate values in a list
+values_list = ["a", "b", "a", "c", "d", "e", "f", "b"]
+
+duplicate_values = []
+
+for value in values_list:
+  if values_list.count(value) > 1:
+    if value not in duplicate_values:
+      duplicate_values.append(value)
+
+print(duplicate_values)
