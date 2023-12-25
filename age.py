8338b324d4859efd36c29f2d6aeee92c42c006f3
Kunal <70285661+kvohra93@users.noreply.github.com>
2023-12-24 23:22:08 -0600

    Create age.py
    exercise to get age from users year of birth

---
+++ age.py
@@ -0,0 +1,7 @@
+# asking user for their year of birth and printing their age
+birth_year = input("what is your year of birth?: ")
+current_year = 2023
+
+age = current_year - int(birth_year)
+
+print(f"you are {age} years old")
